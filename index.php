b869caf9520e1bc61bcd539ec48cbc18e6e476af
Siddharth <89509416+siddharth780@users.noreply.github.com>
2022-03-02 13:01:56 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,222 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title></title>
+        <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/styles.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Bellefair&display=swap" rel="stylesheet">
+    </head>
+    <body>
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Spread Love❤️</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ml-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="about.php">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Services</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contact us</a>
+        </li>
+        
+        
+      </ul>
+      <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+
+<div id="demo" class="carousel slide" data-ride="carousel">
+  <ul class="carousel-indicators">
+    <li data-target="#demo" data-slide-to="0" class="active"></li>
+    <li data-target="#demo" data-slide-to="1"></li>
+    <li data-target="#demo" data-slide-to="2"></li>
+  </ul>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="images/ast1.jpg" alt="Los Angeles" width="1100" height="500">
+      <div class="carousel-caption">
+        <h3>Los Angeles</h3>
+        <p>We had such a great time in LA!</p>
+      </div>   
+    </div>
+    <div class="carousel-item">
+      <img src="images/ast2.jpg" alt="Chicago" width="1100" height="500">
+      <div class="carousel-caption">
+        <h3>Chicago</h3>
+        <p>Thank you, Chicago!</p>
+      </div>   
+    </div>
+    <div class="carousel-item">
+      <img src="images/ast3.jpg" alt="New York" width="1100" height="500">
+      <div class="carousel-caption">
+        <h3>New York</h3>
+        <p>We love the Big Apple!</p>
+      </div>   
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#demo" data-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+  </a>
+  <a class="carousel-control-next" href="#demo" data-slide="next">
+    <span class="carousel-control-next-icon"></span>
+  </a>
+</div>
+
+  <section class="my-5">
+    <div class="py-5">
+    <div>
+      <h2 style="font-weight: bold;" class="text-center">About</h2><br>
+    </div>
+    <div class="container-fluid">
+    <div class="row">
+      <div class="col-lg-5 col-md-6 col-12">
+        <img src="images/a.jpg" class="img-fluid aboutimg">
+      </div>
+      <div class="col-lg-6 col-md-6 col-12">
+        <h2 class="display-4">We are "SPREAD LOVE❤️"</h2>
+        <p class="py-3 display-5">Stress is part of being human, and it can help motivate you to get things done.</p>
+        <a href="about.php" class="btn btn-success">know us better :)</a>
+      </div>
+    </div>
+    </div>
+  </section>
+
+  <section class="my-5">
+    <div class="py-5">
+    <div>
+      <h2 style="font-weight: bold;" class="text-center">Services</h2><br>
+    </div>
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-lg-4 col-md-4 col-12">
+          <div class="card">
+            <img class="card-img-top" src="images/smile.jpg" alt="Card image">
+            <div class="card-body">
+              <h4 class="card-title">Keep smiling :)</h4>
+              <p class="card-text"></p>
+              <a href="#" class="btn btn-primary"></a>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-lg-4 col-md-4 col-12">
+          <div class="card">
+            <img class="card-img-top" src="images/dowhatyoulove.jpg" alt="Card image">
+            <div class="card-body">
+              <h4 class="card-title"></h4>
+              <p class="card-text"></p>
+              <a href="#" class="btn btn-primary"></a>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-lg-4 col-md-4 col-12">
+          <div class="card">
+            <img class="card-img-top" src="images/enjoylife.jpg" alt="Card image">
+            <div class="card-body">
+              <h4 class="card-title">Enjoy Life!</h4>
+              <p class="card-text"></p>
+              <a href="#" class="btn btn-primary"></a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div> 
+</section> 
+
+
+<section class="my-5">
+    <div class="py-5">
+    <div>
+      <h2 style="font-weight: bold;" class="text-center">Feel Better 🥰 </h2><br>
+    </div>
+
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-lg-4 md-4 col-12">
+          <img src="images/aest1.jpg" class="img-fluid pb-3">
+        </div>
+        <div class="col-lg-4 md-4 col-12">
+          <img src="images/aest2.jpg" class="img-fluid pb-3">
+        </div>
+        <div class="col-lg-4 md-4 col-12">
+          <img src="images/aest3.jpg" class="img-fluid pb-3">
+        </div>
+        <div class="col-lg-4 md-4 col-12">
+          <img src="images/aest4.jpg" class="img-fluid pb-3">
+        </div>
+        <div class="col-lg-4 md-4 col-12">
+          <img src="images/aest5.jpg" class="img-fluid pb-3">
+        </div>
+        <div class="col-lg-4 md-4 col-12">
+          <img src="images/aest6.jpg" class="img-fluid pb-3">
+        </div>
+        </div>
+      </div>
+    </div>
+
+</section>
+
+<section class="my-5">
+    <div class="py-5">
+    <div class="w-50 m-auto">
+      <h2 style="font-weight: bold;" class="text-center">Contact Us</h2><br>
+      <h3 style="font-size: 25px;">Fill your details below to make us know you better :)</h3><br>
+    </div>
+
+    <div class="w-50 m-auto">
+      <form action="userinfo.php" method="post">
+        <div class="form-group">
+          <label>Username</label>
+          <input type="text" name="user" autocomplete="off" class="form-control">
+        </div>
+        <div class="form-group">
+          <label>Email-id</label>
+          <input type="text" name="email" autocomplete="off" class="form-control">
+        </div>
+        <div class="form-group">
+          <label>Mobile Number</label>
+          <input type="number" name="mobile" autocomplete="off" class="form-control">
+        </div>
+        <div class="form-group">
+          <label>Anything you would like to share with us?</label>
+          <textarea class="form-control">
+
+          </textarea>
+          </div>
+          <button type="submit" class="btn btn-success">Submit</button>
+      </form>
+    </div>
+
+</section>
+<footer>
+<p  class="p-3 bg-dark text-white text-center">Visit us - @wearespreadlove@gmail.com</p>
+</footer>
+
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
+
+
+
+
+    </body>
+</html>
